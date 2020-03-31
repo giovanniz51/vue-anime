4352e24f894ccd3c3a29fd52d82f264330a40698
EC2 Default User <ec2-user@ip-172-31-41-101.eu-central-1.compute.internal>
2020-03-31 16:59:36 +0000

    added nprogress

---
+++ precache-manifest.1d18e3b4da4e7e1754ebdcc1d5b42f3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fdcf138b8de3a6077c",
+    "url": "/vue-anime/css/about.1b6d797c.css"
+  },
+  {
+    "revision": "7e68fc31ffab58083078",
+    "url": "/vue-anime/css/app.d833ff3e.css"
+  },
+  {
+    "revision": "c3bb19530c7defc0d57b",
+    "url": "/vue-anime/css/chunk-vendors.b755446a.css"
+  },
+  {
+    "revision": "68c3a5054502a9068b20eda9f7515261",
+    "url": "/vue-anime/index.html"
+  },
+  {
+    "revision": "18fdcf138b8de3a6077c",
+    "url": "/vue-anime/js/about.5535f8ee.js"
+  },
+  {
+    "revision": "7e68fc31ffab58083078",
+    "url": "/vue-anime/js/app.bb1d9fb3.js"
+  },
+  {
+    "revision": "c3bb19530c7defc0d57b",
+    "url": "/vue-anime/js/chunk-vendors.fb08cbc2.js"
+  },
+  {
+    "revision": "0bd0684503084255c6b33b2649969c80",
+    "url": "/vue-anime/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-anime/robots.txt"
+  }
+]);
